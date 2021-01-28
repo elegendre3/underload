0750cdbcb80ecae4d93de3b286a8e5da824303a2
Eliott Legendre <eliott.legendre@imanage.com>
2021-01-28 18:56:50 +0100

    gitignore and interest

---
+++ src/main/python/helpers/interests.py
@@ -0,0 +1,15 @@
+# CULTURE
+class Culture:
+    persons = ["kamal ravikant", 'carl sagan', 'malcolm gladwell', 'dan brown', 'tim ferris', 'lex fridman']
+    topics = ['sleep', 'meditation', 'yoga', 'latest movies']
+
+
+# SPORTS
+class Sports:
+    persons = []
+    topics = ['formula one', 'cycling', 'cyclisme', 'triathlon', 'running', 'bouldering']
+
+
+# SCIENCE
+class Science:
+    topics = ['natural language processing', 'pytorch', 'tensorflow', 'papers with code', 'transformers', 'huggingface']
