ab245d987dcb31a2fa4e5d24865461aedfd34e72
Eliott Legendre <eliott.legendre@imanage.com>
2021-01-28 19:26:55 +0100

    refactor a bit

---
+++ src/main/python/news/news_site/api/news_api/key.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import shutil
+
+
+def copy_api_key():
+    try:
+        p = Path('/Users/eliott.legendre/Desktop/news_api.secret')
+        target = f'/tmp/{p.name}'
+        shutil.copy(p, target)
+    except FileNotFoundError:
+        raise FileNotFoundError('Api Key secret file not found.')
+
+    print(f'Api Key secret file copied to [{target}]')
+    return 0
+
+
+def read_api_key(path: Path = Path('/tmp/news_api.secret')):
+    with path.open('r') as f:
+        api_key = f.read()
+    return api_key
+
+
+def get_key():
+    try:
+        api_key = read_api_key()
+    except FileNotFoundError:
+        copy_api_key()
+        api_key = read_api_key()
+    return api_key
